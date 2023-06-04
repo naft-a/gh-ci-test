a7e47033538dcb891b23966159e900c6297a7cfb
Georgi Ganchev <googs.bs@gmail.com>
2023-06-04 11:04:07 +0100

    inital commit

---
+++ server_test.rb
@@ -0,0 +1,17 @@
+require "rack"
+require "rack/test"
+require "test/unit"
+require "json"
+
+class TestApp < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Rack::Builder.parse_file("config.ru")
+  end
+
+  def test_root
+    get "/"
+    assert last_response.ok?
+  end
+end
